7802ffc0d4a2270c43dd896f46db27f98ac496e5
Fernando <fernando@sisdado.com>
2015-02-03 10:47:34 -0200

    teste03-fevereiro

---
+++ app/model/ColetaPneus_m.js
@@ -0,0 +1,11 @@
+Ext.define('Touch2Demo.model.ColetaPneus_m', {
+    extend: 'Ext.data.Model',
+    config: {
+	fields: [
+	    {name: 'cod_pneu'},
+	    {name:'codColeta'},
+	    {name:'servico'}
+	]
+    }
+});
+
